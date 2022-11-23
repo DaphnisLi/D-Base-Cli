bc2e54ee132027b1afb040e565c825031969477e
DaphnisLi <1872758972@qq.com>
2022-11-23 15:41:56 +0800

    fix: 补充 commit 注释

--- src/order/create/template.ts
+++ src/order/create/template.ts
@@ -158,7 +158,16 @@ trim_trailing_whitespace = true
 insert_final_newline = true
 `
 
-export const CommitlintConfigJs = `module.exports = {
+export const CommitlintConfigJs = `// feat: 新特性
+// fix: 修改问题
+// refactor: 代码重构
+// docs: 文档修改
+// style: 样式修改 (我知道是代表代码格式不代表样式, 但我就用它来代表样式, so what!)
+// test: 测试用例修改
+// chore: 构建流程、依赖管理
+// revert: revert 前一个 commit
+// release: 发布 NPM
+module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'release', 'chore']],
